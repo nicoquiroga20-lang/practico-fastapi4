afd1038a36cdfa6a740524decf3cba5cbbd28b2c
Quiroga Mauricio Nicolás <nicoquiroga20@gmail.com>
2026-06-24 23:07:01 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routers.articulos import router as articulos_router
 
-app = FastAPI() # se instancia la app de FastAPI
+app = FastAPI()
 app.title = 'TP Evaluativo - Mi API'
 
 app.add_middleware(
